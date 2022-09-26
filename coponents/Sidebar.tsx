e03bcdd5f9aa90c6f64a90e551a6e8788535c86c
Mohammad Totonchy <103252562+Mohtoto@users.noreply.github.com>
2022-09-27 04:07:37 +0800

    first files

---
+++ coponents/Sidebar.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import {
+  BellIcon ,
+  HashtagIcon,
+  BookmarkIcon ,
+  // CollectionIcon,
+  // DotsCircleHorizontalIcon,
+  // MailIcon,
+  UserIcon,
+  HomeIcon,
+}from '@heroicons/react/24/outline'
+
+function Sidebar() {
+  return (
+    <div>
+      <img className='h-10 w-10' src="https://links.papareact.com/drq" alt="" />
+
+    </div>
+  )
+}
+
+export default Sidebar
